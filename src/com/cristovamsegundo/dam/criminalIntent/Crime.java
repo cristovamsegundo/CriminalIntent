18626377e78d5382a778462071c26c02383561c7
cristovamsegundo <cristov4m445@gmail.com>
2014-10-03 15:10:37 -0300

    Second version.
    New Button that displays the date, a "solved" CheckBox that indicates
    whether the problem was solved and two new titles. A landscape xml was
    added as well. Some changes on Crime and CrimeFragment to support the
    new widgets.

--- src/com/cristovamsegundo/dam/criminalIntent/Crime.java
+++ src/com/cristovamsegundo/dam/criminalIntent/Crime.java
@@ -1,5 +1,6 @@
 package com.cristovamsegundo.dam.criminalIntent;
 
+import java.util.Date;
 import java.util.UUID;
 
 import android.util.Log;
@@ -7,11 +8,15 @@ import android.util.Log;
 public class Crime {
 	private UUID mId;
 	private String mTitle;
+	private Date mDate;
+	private boolean mSolved;
 	private static final String TAG = "CrimeActivity";
 	
 	public Crime() {
 		// Generate unique identifier
 		mId = UUID.randomUUID() ;
+		// Gets the current date
+		mDate = new Date();
 	}
 
 	public UUID getId() {
@@ -26,6 +31,24 @@ public class Crime {
 		this.mTitle = mTitle;
 		Log.d(TAG, mTitle);
 	}
+
+	public Date getDate() {
+		return mDate;
+	}
+
+	public void setDate(Date date) {
+		this.mDate = date;
+	}
+
+	public boolean ismSolved() {
+		return mSolved;
+	}
+
+	public void setmSolved(boolean solved) {
+		this.mSolved = solved;
+	}
+	
+	
 	
 
 }
--- src/com/cristovamsegundo/dam/criminalIntent/CrimeFragment.java
+++ src/com/cristovamsegundo/dam/criminalIntent/CrimeFragment.java
@@ -6,12 +6,17 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 
 public class CrimeFragment extends Fragment {
 	private Crime mCrime;
 	private EditText mTitleView;
+	private Button mDateButton;
+	private CheckBox mSolvedCheckBox;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
@@ -37,6 +42,21 @@ public class CrimeFragment extends Fragment {
 				// This one too
 			}
 			
+		});
+		
+		mDateButton = (Button)v.findViewById(R.id.crime_date);
+		mDateButton.setText(mCrime.getDate().toString());
+		mDateButton.setEnabled(false);
+		
+		mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
+		mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				// Set the crime's solved property
+				mCrime.setmSolved(isChecked);
+				
+			}
 		});
 		
 		return v;
