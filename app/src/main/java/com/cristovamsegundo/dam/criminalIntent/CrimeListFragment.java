c30efa20b64ca9deb084fab590455927a303cf2a
Cristovam Segundo <cristov4m445@gmail.com>
2014-10-15 03:20:26 -0300

    Version 5
    Swipe navigation working and title changing depending on the selected crime.

--- app/src/main/java/com/cristovamsegundo/dam/criminalIntent/CrimeListFragment.java
+++ app/src/main/java/com/cristovamsegundo/dam/criminalIntent/CrimeListFragment.java
@@ -36,8 +36,8 @@ public class CrimeListFragment extends ListFragment{
         // Get the crime from the adapter
         Crime c = ((CrimeAdapter)getListAdapter()).getItem(position);
 
-        // Start CrimeActivity
-        Intent i = new Intent(getActivity(), CrimeActivity.class);
+        // Start CrimePagerActivity with this crime
+        Intent i = new Intent(getActivity(), CrimePagerActivity.class);
         i.putExtra(CrimeFragment.EXTRA_CRIME_ID, c.getId());
         startActivity(i);
     }
