901a124cc106f3e17ec51eef61db4d28082597d4
cristovamsegundo <cristov4m445@gmail.com>
2014-11-05 17:55:48 -0300

    Version 9.5
    CrimeCameraFragment implemented but not connected yet.

---
+++ src/com/cristovamsegundo/dam/criminalIntent/CrimeCameraFragment.java
@@ -0,0 +1,126 @@
+package com.cristovamsegundo.dam.criminalIntent;
+
+import java.io.IOException;
+import java.util.List;
+
+import android.annotation.TargetApi;
+import android.hardware.Camera;
+import android.hardware.Camera.Size;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class CrimeCameraFragment extends Fragment {
+
+	private static final String TAG = "CrimeCameraFragment";
+	
+	private Camera mCamera;
+	private SurfaceView mSurfaceView;
+	
+	@Override
+	@SuppressWarnings("deprecation")
+	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState){
+		View v = inflater.inflate(R.layout.fragment_crime_camera, parent, false);
+		
+		Button takePictureButton = (Button) v.findViewById(R.id.crime_camera_takePictureButton);
+		takePictureButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				getActivity().finish();
+			}
+		});
+		
+		mSurfaceView = (SurfaceView) v.findViewById(R.id.crime_camera_surfaceView);
+		SurfaceHolder holder = mSurfaceView.getHolder(); // Used to work on previous versions
+		holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS); // Used to work on previous versions
+		
+		holder.addCallback(new SurfaceHolder.Callback() {
+			
+			@Override
+			public void surfaceDestroyed(SurfaceHolder holder) {
+				// We can no longer display on this surface, so stop preview.
+				if(mCamera != null){
+					mCamera.stopPreview();
+				}
+				
+			}
+			
+			@Override
+			public void surfaceCreated(SurfaceHolder holder) {
+				// Tell the camera to use this holder as its preview area
+				try{
+					if(mCamera != null){
+						mCamera.setPreviewDisplay(holder);
+					}
+				} catch(IOException exception){
+					Log.e(TAG, "Error setting up preview display", exception);
+				}
+				
+			}
+			
+			@Override
+			public void surfaceChanged(SurfaceHolder holder, int format, int width,
+					int height) {
+				if(mCamera == null) {return;}
+				
+				Camera.Parameters parameters = mCamera.getParameters();
+				Size s = getBestSupportedSize(parameters.getSupportedPreviewSizes(), width, height);
+				parameters.setPreviewSize(s.width, s.height);
+				mCamera.setParameters(parameters);
+				try{
+					mCamera.startPreview();
+				}catch(Exception e){
+					Log.e(TAG, "Could not start preview", e);
+					mCamera.release();
+					mCamera = null;
+				}
+				
+			}
+		});
+		
+		return v;
+	}
+	
+	private Size getBestSupportedSize(List<Size> sizes, int width, int height){
+		Size bestSize = sizes.get(0);
+		int largestArea = bestSize.width * bestSize.height;
+		for(Size s:sizes){
+			int area = s.width * s.height;
+			if(area > largestArea){
+				bestSize = s;
+				largestArea = area;
+			}
+		}
+		return bestSize;
+	}
+	
+	@TargetApi(9)
+	@Override
+	public void onResume(){
+		super.onResume();
+		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD){
+			mCamera = Camera.open(0);
+		} else{
+			mCamera = Camera.open();
+		}
+	}
+	
+	@Override
+	public void onPause(){
+		super.onPause();
+		
+		if(mCamera != null){
+			mCamera.release();
+			mCamera = null;
+		}
+	}
+	
+}
